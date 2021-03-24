aa0d2b916c6a4ee922ca5c60537a66fa92d74e29
hunghh <hunghh.dev@gmail.com>
2021-03-24 09:16:51 +0700

    loop

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  products = [
+    {
+      name: 'PRODUCT ITEM NUMBER 1',
+      desc: 'Description for product item number 1',
+      thumbnail: 'https://via.placeholder.com/200x150',
+      price: 5.99,
+      quantity: 2
+    },
+    {
+      name: 'PRODUCT ITEM NUMBER 2',
+      desc: 'Description for product item number 2',
+      thumbnail: 'https://via.placeholder.com/200x150',
+      price: 9.99,
+      quantity: 2
+    },
+    {
+      name: 'PRODUCT ITEM NUMBER 3',
+      desc: 'Description for product item number 3',
+      thumbnail: 'https://via.placeholder.com/200x150',
+      price: 9.99,
+      quantity: 2
+    }
+  ]
+
 
   constructor() { }
 
