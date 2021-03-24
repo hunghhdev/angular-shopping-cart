09806af6334e0c535c231f00bf493f1a6f4bb047
hunghh <hunghh.dev@gmail.com>
2021-03-24 09:16:36 +0700

    data biding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title = 'angular-shopping-cart';
+  numberItems = 4;
 
   constructor() { }
 
